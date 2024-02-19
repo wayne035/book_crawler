8a6750f979d86f788f39d80a6ea7cc1f197068da
wayne <QWE24367705@gmail.com>
2024-02-19 15:55:04 +0800

    建立books route

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
+from database.books import find_all_data
 import os
 
 load_dotenv()
@@ -9,9 +10,9 @@ app = Flask(__name__)
 app.json.ensure_ascii = False
 CORS(app, resources={r'/.*': {'origins':f'{os.environ.get("URL")}'}})
 
-@app.route('/')
-def home():
-    return 'hi flask'
+@app.route('/books')
+def books():
+    return find_all_data()
 
 if __name__ == '__main__':
     app.run(debug=True)
